5e910f2cd95e746200f5d209edcb477d632f0c42
Mika <mica@morph.fi>
2022-02-14 11:30:03 +0200

    Tilanne osan 2. Display a List jälkeen (https://angular.io/tutorial/toh-pt2)

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Hayder Abduljabbar Al-Hmedavi' },
+    { id: 12, name: 'Milan Jaff' },
+    { id: 13, name: 'Abderrahman Bouanane' },
+    { id: 14, name: 'Qasim Hammood Beni Said Ali' },
+    { id: 15, name: 'Ali Ahmed Hussein' },
+    { id: 16, name: 'Seydoy Kouanda' },
+    { id: 17, name: 'Rami Adham' },
+    { id: 18, name: 'Ramin Azimi' },
+    { id: 19, name: 'Alaa Kadhim Mahdi Albu-Salih' },
+    { id: 20, name: 'Fahad Firas al-Nuaimi'}
+];

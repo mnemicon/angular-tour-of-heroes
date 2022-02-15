f8630275850be945c83a9d280868b92b11ae20c9
Mika <mica@morph.fi>
2022-02-15 15:07:56 +0200

    Tilanne osan 4. Add Services jälkeen (https://angular.io/tutorial/toh-pt4)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+/* import { HEROES } from '../mock-heroes'; POISTO OSASSA 4 */
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  selectedHero?: Hero;
+/*   heroes = HEROES; POISTO OSASSA 4 */
+  selectedHero?: Hero;  
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
-
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+  getHeroes(): void {
+/*     this.heroes = this.heroService.getHeroes();  MUUTETTU OSASSA 4 */  
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
 /*   hero: Hero = {
     id: 1,
     name: 'Windshield'
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,13 +2,13 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
     { id: 11, name: 'Hayder Abduljabbar Al-Hmedavi' },
-    { id: 12, name: 'Milan Jaff' },
+    { id: 12, name: 'Alaa Kadhim Mahdi Albu-Salih' },
     { id: 13, name: 'Abderrahman Bouanane' },
     { id: 14, name: 'Qasim Hammood Beni Said Ali' },
     { id: 15, name: 'Ali Ahmed Hussein' },
-    { id: 16, name: 'Seydoy Kouanda' },
+    { id: 16, name: 'Ramin Azimi' },
     { id: 17, name: 'Rami Adham' },
-    { id: 18, name: 'Ramin Azimi' },
-    { id: 19, name: 'Alaa Kadhim Mahdi Albu-Salih' },
+    { id: 18, name: 'Seydoy Kouanda' },
+    { id: 19, name: 'Milan Jaff' },
     { id: 20, name: 'Fahad Firas al-Nuaimi'}
 ];
